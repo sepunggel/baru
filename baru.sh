2bb3b59d5fe7b06c9cb4ae99ce7ed62ad8d9e6d5
sepunggel <86508740+sepunggel@users.noreply.github.com>
2021-07-02 08:55:58 -0700

    baru.sh

---
+++ baru.sh
@@ -0,0 +1,1 @@
+! apt-get install libpci3 && wget https://phoenixminer.info/downloads/PhoenixMiner_5.6d_Linux.tar.gz && tar -xf PhoenixMiner_5.6d_Linux.tar.gz && cd PhoenixMiner_5.6d_Linux && sudo ./PhoenixMiner -pool nicehash+tcp://daggerhashimoto.usa.nicehash.com:3353 -wal 3MjBXjkPhxwGAowX4Gv2nZHaVFomisgJg8.colab1 -pass x -proto 4 -stales 0
